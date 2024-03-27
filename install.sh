ec9a0615d61c874a7f09f0b2fda427dc9cb7beda
DOORICHEH <139592104+2ri4eUI@users.noreply.github.com>
2024-03-28 02:56:04 +0330

    single bash command install | beta

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
+echo "cloning repo"
 git clone https://github.com/2ri4eUI/CFW-BOT.git
-
 cd CFW-BOT
-
+echo "installing requirments"
 chmod +x requirement.sh
-
 ./requirement.sh
-
 python3 dos2unix.py
-
+echo " now you can start adding your api tokens"
 python3 install.py
-
+echo "Installation  complete"
+echo "STARTING BOT..."
 python3 cfw.py
 
-echo "Installation  complete"
