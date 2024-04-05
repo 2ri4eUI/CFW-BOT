15f864f36a6f765f7def6dd8ec09db5c07a2dfec
DOORICHEH <139592104+2ri4eUI@users.noreply.github.com>
2024-04-05 23:19:13 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-echo "cloning repo"
-git clone https://github.com/2ri4eUI/CFW-BOT.git
-cd CFW-BOT
-echo "installing requirments"
-python3 dos2unix.py
-echo "requirement passed"
-chmod +x requirement.sh
-./requirement.sh
-python3 dos2unix.py
-echo "requirement passed"
-chmod +x requirement.sh
-./requirement.sh
-echo " now you can start adding your api tokens"
-python3 install.py
-echo "Installation  complete"
-echo "STARTING BOT..."
-python3 cfw.py
 
+echo "Cloning repo..."
+git clone https://github.com/2ri4eUI/CFW-BOT.git
+
+
+cd CFW-BOT
+
+echo "Installing requirements..."
+
+
+if [ -f "dos2unix.py" ]; then
+    python3 dos2unix.py
+    echo "dos2unix.py executed"
+fi
+
+if [ -f "requirement.sh" ]; then
+
+    python3 dos2unix.py requirement.sh
+    echo "requirement.sh converted to UNIX format"
+    
+    chmod +x requirement.sh
+    ./requirement.sh
+    echo "requirement.sh executed"
+fi
+
+echo "Now you can start adding your API tokens..."
+
+python3 install.py
+
+echo "Installation complete"
+echo "Starting bot..."
+
+python3 cfw.py
