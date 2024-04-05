644b8b54a6da9531d88bd0a0870358634885ae76
DOORICHEH <139592104+2ri4eUI@users.noreply.github.com>
2024-04-05 23:05:28 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ echo "cloning repo"
 git clone https://github.com/2ri4eUI/CFW-BOT.git
 cd CFW-BOT
 echo "installing requirments"
+python3 dos2unix.py
 chmod +x requirement.sh
 ./requirement.sh
-python3 dos2unix.py
 echo " now you can start adding your api tokens"
 python3 install.py
 echo "Installation  complete"
