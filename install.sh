bcd9abbab815f2ce946429e1a8a9fda46ed904b7
DOORICHEH <139592104+2ri4eUI@users.noreply.github.com>
2024-04-05 23:51:56 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-echo "Cloning repo..."
-git clone https://github.com/2ri4eUI/CFW-BOT.git
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' 
 
+CHECK_MARK='✅'
+WARNING='⚠️'
+ROCKET='🚀'
+
+echo -e "${YELLOW}Cloning repo...${NC}"
+git clone https://github.com/2ri4eUI/CFW-BOT.git
 
 cd CFW-BOT
 
-echo "Installing requirements..."
-
+echo -e "${YELLOW}Installing requirements...${NC}"
 
 if [ -f "dos2unix.py" ]; then
     python3 dos2unix.py
-    echo "dos2unix.py executed"
+    echo -e "${GREEN}${CHECK_MARK} dos2unix.py executed${NC}"
 fi
 
 if [ -f "requirement.sh" ]; then
 
     python3 dos2unix.py requirement.sh
-    echo "requirement.sh converted to UNIX format"
+    echo -e "${GREEN}${CHECK_MARK} requirement.sh converted to UNIX format${NC}"
     
 
-    echo -n "Installing requirements: "
+    echo -e "${YELLOW}Installing requirements: ${NC}\c"
     for i in {1..10}; do
-        echo -n "."
-        sleep 1  # Simulate installation time
+        echo -e -n "${GREEN}.${NC}"
+        sleep 1  
     done
-    echo " [Done]"
+    echo -e " ${GREEN}${CHECK_MARK}${NC}"
 fi
 
-clear
-echo "Now you can start adding your API tokens..."
+echo -e "${YELLOW}Now you can start adding your API tokens...${NC}"
 
 python3 install.py
 
-echo "Installation complete"
-echo "Starting bot..."
+echo -e "${GREEN}${ROCKET} Installation complete${NC}"
+echo -e "${GREEN}${ROCKET} Starting bot...${NC}"
 
 python3 cfw.py
